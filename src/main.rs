2eab929599e42cf48db2fb424dcc84185e3abe04
Michael Raymond <mraymond@hpe.com>
2026-02-17 14:27:18 -0600

    Initial checkin
    The MapGraph struct can add Edges and Vertexes.

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+
+#[derive(PartialEq, Eq, Hash)]
+struct Vertex {
+    name : String,
+}
+
+struct Edge {
+    p0 : Rc<Vertex>,
+    p1 : Rc<Vertex>,
+    distance: f32,
+}
+
+struct MapGraph {
+    vertices: HashMap<String, Rc<Vertex>>,
+    edges: HashMap<(String, String), Edge>,
+}
+
+impl MapGraph {
+    fn new() -> MapGraph {
+        MapGraph {vertices: HashMap::new(), edges: HashMap::new()}
+    }
+
+    fn insert_vertices(&mut self, verts: &[&str]) {
+        for i in 0..verts.len() {
+            println!("{}", verts[i]);
+
+            let v: Rc<Vertex> = Vertex {
+                name: String::from(verts[i]),
+            }.into();
+
+            self.vertices.insert(v.name.clone(), v);
+        }
+    }
+
+    fn insert_edges(&mut self, edges: &[(&str, &str, f32)]) {
+        for i in 0..edges.len() {
+            let (p0, p1, d) = edges[i];
+            println!("{} {}", p0, p1);
+
+            let v0 = self.vertices.get(p0).
+                expect("Couldn't find the first vertex");
+            let v1 = self.vertices.get(p1).
+                expect("Couldn't find the second vertex");
+
+            let e = Edge {p0: v0.clone(), p1: v1.clone(), distance: d};
+            self.edges.insert((p0.to_string(), p1.to_string()), e);
+        }
+    }
+}
+
+fn main() {
+    let input_verts: [&str; 3] = ["A", "B", "C"];
+    let input_edges: [(&str, &str, f32); 3] = [
+        ("A", "B", 3.0), ("A", "C", 1.0), ("C", "B", 1.5)
+    ];
+    let mut graph = MapGraph::new();
+
+    graph.insert_vertices(&input_verts);
+    graph.insert_edges(&input_edges);
+
+    for (_, e) in graph.edges.iter() {
+        println!("Edge {} {} is {}",
+                 e.p0.name, e.p1.name, e.distance);
+    }
+
+    
+}
